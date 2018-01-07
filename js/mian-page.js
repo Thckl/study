fa5260333b54e0c980157be29f25f6eaf8b95464
XZD <315263445@qq.com>
2018-01-07 20:52:52 +0800

    游戏中心二级菜单完成

---
+++ js/mian-page.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+        var t;
+        // $(".nav-li1").hover(function () {
+        //     clearTimeout(t);
+        //     t = setTimeout(function () {
+        //         $(".nav-li1").find(".card-nav").animate({
+        //             'top': 40 + 'px',
+        //             'opacity': 1
+        //         }, 200).addClass("dis");
+        //     }, 500);
+        // });
+    
+        // $(".nav-li1").mouseleave(function () {
+        //     clearTimeout(t);
+        //     $(this).find(".card-nav").animate({
+        //         'top': 50 + 'px',
+        //         'opacity': 0
+        //     }, 200, function () {
+        //         $(".nav-li1").find(".card-nav").removeClass("dis");
+        //     });
+        // });
+$(function(){
+            $(".nav-ul li").hover(
+                function(){
+                    var is = $(this);
+                    t=setTimeout(function(){
+                    	
+                        $(is).children("ul").animate({
+                            'top': 40 + 'px',
+                            'opacity': 1
+                        }, 200)
+                        $(is).children("ul").show();
+                    },1000);
+                },
+                function(){
+                    clearTimeout(t);
+                    
+                    $(this).children("ul").animate({
+                        'top': 50 + 'px',
+                        'opacity' : 0
+                    }, 200)
+                    $(this).children("ul").hide();
+                    
+                }
+                );
+        });
